7b84945d42d198a5141ef0166e2db9226f19cf33
Mac - gms <gms771218@gamil.com>
2018-05-30 20:44:38 +0800

    modify WeekVO.class

--- src/main/java/com/gms/util/date/model/WeekVO.java
+++ src/main/java/com/gms/util/date/model/WeekVO.java
@@ -66,6 +66,30 @@ public class WeekVO {
             return day;
         }
 
+        public String getDayString(){
+            return String.format("%04d-%02d-%02d" , year , month , day ) ;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Day)) return false;
+
+            Day day1 = (Day) o;
+
+            if (year != day1.year) return false;
+            if (month != day1.month) return false;
+            return day == day1.day;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = year;
+            result = 31 * result + month;
+            result = 31 * result + day;
+            return result;
+        }
+
         @Override
         public String toString() {
             return "Day{" +
