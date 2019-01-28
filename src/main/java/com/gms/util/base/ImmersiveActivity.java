16a14d07f23ebf3d46a02a82331e225151a9957d
Mac - gms <gms771218@gamil.com>
2019-01-29 00:36:48 +0800

    1. 重構沉浸式ImmersiveActivity.java 2. 修正gradle參數

--- src/main/java/com/gms/util/base/ImmersiveActivity.java
+++ src/main/java/com/gms/util/base/ImmersiveActivity.java
@@ -1,15 +1,17 @@
 package com.gms.util.base;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.gms.util.device.Screen;
 
 /**
+ * Modify by gms on 2019/01/29.
+ * 修正沉浸式Activity
+ *
  * Created by gms on 2017/10/20.
  * <p>
  * 沉浸式Activity
@@ -18,12 +20,120 @@ import com.gms.util.device.Screen;
 
 public class ImmersiveActivity extends AppCompatActivity {
 
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+     */
+    private static boolean AUTO_HIDE = true;
+
+    /**
+     * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static int UI_ANIMATION_DELAY = 300;
+
+    private Handler mHideHandler = new Handler();
+    private Runnable mHidePart2Runnable = new Runnable() {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        @Override
+        public void run() {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LOW_PROFILE
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+
+    private boolean mVisible = false;
+    private Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+
+    private void hide() {
+        // Hide UI first
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    private void show() {
+        // Show the system bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+    }
+
+
+    // ------- ------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mVisible = true;
         Screen.Initialize(this);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        hide();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
 
     @Override
     protected void onDestroy() {
@@ -31,71 +141,77 @@ public class ImmersiveActivity extends AppCompatActivity {
         Screen.Destroy();
     }
 
-    static Handler handler = new Handler();
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        hideSystemUi();
-                    }
-                });
-            }
-        }, 2000);
-    }
-
-    @Override
+        @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus)
-        hideSystemUi();
-//        else {
-//            View decorView = getWindow().getDecorView();
-//            decorView.setOnSystemUiVisibilityChangeListener(null);
-//        }
+        hide();
     }
 
-    public void hideSystemUi() {
-        final View decorView = setSystemUiVisibilityMode();
-        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                setSystemUiVisibilityMode(); // Needed to avoid exiting immersive_sticky when keyboard is displayed
-            }
-        });
-    }
-
-    protected View setSystemUiVisibilityMode() {
-
-        View decorView = getWindow().getDecorView();
-
-        int options;
-        options =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION   // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN        // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-//        decorView.setSystemUiVisibility(options);
-
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-        return decorView;
-    }
+//    static Handler handler = new Handler();
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        hideSystemUi();
+//                    }
+//                });
+//            }
+//        }, 2000);
+//    }
+//
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+////        if (hasFocus)
+//        hideSystemUi();
+////        else {
+////            View decorView = getWindow().getDecorView();
+////            decorView.setOnSystemUiVisibilityChangeListener(null);
+////        }
+//    }
+//
+//    public void hideSystemUi() {
+//        final View decorView = setSystemUiVisibilityMode();
+//        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+//            @Override
+//            public void onSystemUiVisibilityChange(int visibility) {
+//                setSystemUiVisibilityMode(); // Needed to avoid exiting immersive_sticky when keyboard is displayed
+//            }
+//        });
+//    }
+//
+//    protected View setSystemUiVisibilityMode() {
+//
+//        View decorView = getWindow().getDecorView();
+//
+//        int options;
+//        options =
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION   // hide nav bar
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN        // hide status bar
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//
+////        decorView.setSystemUiVisibility(options);
+//
+//        decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//
+//        return decorView;
+//    }
 
 }
